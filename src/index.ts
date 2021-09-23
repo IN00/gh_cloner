316cbde3b4499111fe69709068b70ac4e32b656c
Naoya Iwashita <>
2021-09-24 01:08:08 +0900

    rename current_direntries.ts to current_directory.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import childProcess from 'child_process'
 import GhClonerRepoList from './value_object/gh_cloner_repo_list'
-import CurrentDirectory from './value_object/current_direntries'
+import CurrentDirectory from './value_object/current_directory'
 
 /**
  * @description
